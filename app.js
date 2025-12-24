2d6173f1a77cf2a02ac5b01af6b9e6f692445614
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-24 23:52:03 +0100

    Tried somethin in js but failed so I will just keep it this way

--- app.js
+++ app.js
@@ -335,7 +335,7 @@ function renderNotes(){
 
     sortedDates.forEach(d => {
         const div = document.createElement("div");
-        div.className = "bg-gray-800 border-gray-700 rounded-lg p-3";
+        div.className = "bg-gray-800 border-gray-700 rounded-lg p-3 flex justify-between items-start";
 
         const left = document.createElement("div");
         const dateP = document.createElement("p");
@@ -377,9 +377,29 @@ function renderNotes(){
                 if(noteDate.value ===d) noteText.value = "";
             }
         });
-        
+
+
+        const saveBtn = document.createElement("button");
+        saveBtn.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.5.5 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+        </svg>`;
+        saveBtn.className ="p-1 rounded hover:bg-gray-700 transition text-green-400 hover:text-green-300";
+        saveBtn.addEventListener("click", () => {
+            state.notes[noteDate.value] = noteText.value;
+            saveState();
+            renderNotes();
+        })
+        right.appendChild(editBtn);
+        right.appendChild(delBtn);
+        right.appendChild(saveBtn);
+
+        div.appendChild(left);
+        div.appendChild(right);
+
         notesList.appendChild(div);
     });
+
+    
 }
 
 //load note for the actively chosen date
