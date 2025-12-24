4eb38dfe63787427718f865aaf589c9c7399b6f7
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-24 17:46:37 +0100

    Set up the setupTabs() function where we go through buttons an sections to see if they are going to be displayed and how

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ let state = {
 
 
 
+function setupTabs() {
+    const buttons = document.querySelectorAll("[data-tab]");
+    const sections = document.querySelectorAll(".tab-section");
+
+    buttons.forEach(btn => {
+        btn.addEventListener("click", () => {
+            const target = btn.dataset.tab;
+
+            sections.forEach(sec => {
+                sec.classList.toggle("hidden",sec.id !==target);
+            });
+
+            buttons.forEach(b => b.classList.remove("bg-gray-700"));
+        });
+    });
+}
