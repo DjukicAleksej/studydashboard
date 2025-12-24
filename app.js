fd1c1270f3285f8e390f11557220ae5362fff8e7
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-24 21:32:00 +0100

    Added delete icon from bootstrap , finished the logic of renderTests

--- app.js
+++ app.js
@@ -236,9 +236,36 @@ function renderTests(){
         date.className=`text-sm ${isPast ? "text-rad-400" : "text-green-400"}`;
         date.textContent = test.date;
 
-        
-    })
+        info.appendChild(title);
+        info.appendChild(date);
+
+        const del = document.createElement("button");
+        del.innerHTML=`
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="32"
+     height="32"
+     fill="currentColor"
+     viewBox="0 0 16 16">
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+</svg>
+`;
+        del.className= "p-2 rounded-full hover:bg-gray-700 transition text-gray-400 hover:text-red-400";
+        del.addEventListener("click", () => {
+            state.tests = state.tests.filter(t=> t.id !== test.id);
+            saveState();
+            renderTests();
+            renderDashboard();
+        });
+
+        card.appendChild(info);
+        card.appendChild(del);
+
+        testsList.appendChild(card);
+
+
+    });
 }
+
 function renderNotes(){}
 
 document.addEventListener("DOMContentLoaded", () => {
